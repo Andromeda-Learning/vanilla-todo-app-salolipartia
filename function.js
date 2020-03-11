9ff76b25ac0224c15543fceb4021601ffbc7a34f
salolipartia <salome.lipartia.1@btu.edu.ge>
2020-03-11 09:04:21 +0400

    Created functionality to add new todos in todo list

--- function.js
+++ function.js
@@ -1,25 +1,24 @@
-// Create a new list item when clicking on the "Add" button
+// Creating a new list item when clicking on the "Add" button
 function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
+    const li = document.createElement("li");
+    const inputValue = document.getElementById("myInput").value;
+    const t = document.createTextNode(inputValue);
     li.appendChild(t);
     if (inputValue === '') {
-        alert("You must write something!");
+        alert("Please, write something.");
     } else {
         document.getElementById("myUL").appendChild(li);
     }
     document.getElementById("myInput").value = "";
 
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
+    const span = document.createElement("SPAN");
+    const txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
-
     for (i = 0; i < close.length; i++) {
         close[i].onclick = function() {
-            var div = this.parentElement;
+            const div = this.parentElement;
             div.style.display = "none";
         }
     }
