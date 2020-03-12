fe7158309745ecdb1eb36072d08ef821d3225fed
salolipartia <salome.lipartia.1@btu.edu.ge>
2020-03-13 00:54:39 +0400

    Used <form> to submit entered values

--- function.js
+++ function.js
@@ -1,25 +1,24 @@
-// Creating a new list item when clicking on the "Add" button
-function newElement() {
-    const li = document.createElement("li");
-    const inputValue = document.getElementById("myInput").value;
-    const t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-        alert("Please, write something.");
-    } else {
-        document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
+loadEvents();
+// load every event in the page
+function loadEvents() {
+    document.querySelector('form').addEventListener('submit', submit);
 
-    const span = document.createElement("SPAN");
-    const txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            const div = this.parentElement;
-            div.style.display = "none";
-        }
+}
+// subit data function
+function submit(e) {
+    e.preventDefault();
+    const input = document.querySelector('input');
+    if (input.value == '') {
+        alert('Enter something!')
+    } else {
+        addTask(input.value)
     }
+}
+
+// add tasks
+function addTask(task) {
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(myInput.value))
+    ul.appendChild(li);
 }
